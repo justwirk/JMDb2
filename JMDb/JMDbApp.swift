ef6bb013b2bbeafea1e4bf68b6845b03f7d7e850
justwirk <emreyyilmaz34@gmail.com>
2024-07-31 16:57:28 +0300

    Initial Commit

---
+++ JMDb/JMDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  JMDbApp.swift
+//  JMDb
+//
+//  Created by Emre Yılmaz on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct JMDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
